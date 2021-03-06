cce998437242e3be79e775e66b9e889553d73a57
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-03-06 18:36:01 +0530

    Add files via upload

---
+++ 5.2.py
@@ -0,0 +1,6 @@
+RadiusOfSphree = float(input("Input the radius of the Sphere:"))
+
+pi = 22/7
+
+Volume = (4/3) * pi * RadiusOfSphree *RadiusOfSphree *RadiusOfSphree 
+print("Volume of the sphere:" +str(Volume))
