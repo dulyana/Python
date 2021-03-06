a670cefbb41f775340f9a425b57f5de5c6f58b2b
Dulyana  Gunawardhana <67815837+dulyana@users.noreply.github.com>
2021-03-06 18:35:45 +0530

    Add files via upload

---
+++ 5.1.py
@@ -0,0 +1,10 @@
+RadiusValue = float(input("Enter the radius value of a circle:"))
+
+pi=22/7
+
+Area = pi * RadiusValue * RadiusValue
+
+Circumference=2*pi*RadiusValue
+
+print("Area of the circle is:"+str(Area))
+print("Circumference of the circle is:"+str(Circumference))
